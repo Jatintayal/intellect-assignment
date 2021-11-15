4f69bc3f538cea1405003720bc59c6688a1faf3e
presidio <presidio@presidios-MacBook-Pro.local>
2021-11-15 09:38:05 +0530

    fixed select issue in widget 2

--- src/components/widget-2.js
+++ src/components/widget-2.js
@@ -2,10 +2,22 @@ import React, { memo, useState } from "react";
 
 const Widget2 = () => {
   const [topHighlight, setTopHighlight] = useState(0);
+  const [selectedHighlight, setselectedHighlight] = useState(false);
 
   const showWhite = (num) => {
     return num <= topHighlight;
   };
+
+  const setHighlights = (num, type) => {
+    if (type === 'hover' && num === 0) {
+      setTopHighlight(selectedHighlight)
+    } else if (type === 'click') {
+      setTopHighlight(num)
+      setselectedHighlight(num)
+    } else {
+      setTopHighlight(num)
+    }
+  }
 
   return (
     <div
@@ -26,37 +38,37 @@ const Widget2 = () => {
           transform: "translate(-50%, -50%)",
         }}
         className="widget-2"
-        onMouseLeave={() => setTopHighlight(0)}
+        onMouseLeave={() => setHighlights(0, 'hover')}
       >
-        <div onMouseEnter={() => setTopHighlight(5)}>
+        <div onMouseEnter={() => setHighlights(5, 'hover')} onClick={() => setHighlights(5, 'click')}>
           {showWhite(5) ? (
             <img src="/assets/widget2/Path 4.svg" alt="" width="280px" />
           ) : (
             <img src="/assets/widget2/Path -3.svg" alt="" width="280px" />
           )}
         </div>
-        <div onMouseEnter={() => setTopHighlight(4)}>
+        <div onMouseEnter={() => setHighlights(4, 'hover')} onClick={() => setHighlights(4, 'click')}>
           {showWhite(4) ? (
           <img src="/assets/widget2/Path 5.svg" alt="" width="220px" />
           ) : (
             <img src="/assets/widget2/Path -2.svg" alt="" width="220px" />
           )}
         </div>
-        <div onMouseEnter={() => setTopHighlight(3)}>
+        <div onMouseEnter={() => setHighlights(3, 'hover')} onClick={() => setHighlights(3, 'click')}>
           {showWhite(3) ? (
           <img src="/assets/widget2/Path 6.svg" alt="" width="150px" />
           ) : (
             <img src="/assets/widget2/Path 3.svg" alt="" width="150px" />
           )}
         </div>
-        <div onMouseEnter={() => setTopHighlight(2)}>
+        <div onMouseEnter={() => setHighlights(2, 'hover')} onClick={() => setHighlights(2, 'click')}>
           {showWhite(2) ? (
           <img src="/assets/widget2/Path 7.svg" alt="" width="100px" />
           ) : (
             <img src="/assets/widget2/Path -1.svg" alt="" width="100px" />
           )}
         </div>
-        <div onMouseEnter={() => setTopHighlight(1)}>
+        <div onMouseEnter={() => setHighlights(1, 'hover')} onClick={() => setHighlights(1, 'click')}>
           {showWhite(1) ? (
           <img src="/assets/widget2/Polygon 2.svg" alt="" width="50px" />
           ) : (
